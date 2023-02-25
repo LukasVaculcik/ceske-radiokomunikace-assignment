c28c073f35e9626e735b069f853f1eb78aed5c00
Lukáš Vaculčík <lukas.vaculcik@gmail.com>
2023-02-25 14:56:13 +0100

    register just one main config

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -15,17 +15,16 @@ class Bootstrap
 		$appDir = dirname(__DIR__);
 
 		//$configurator->setDebugMode('secret@23.75.345.200'); // enable for your remote IP
+		$configurator->setDebugMode(true);
 		$configurator->enableTracy($appDir . '/log');
-
+        $configurator->setTimeZone('Europe/Prague');
 		$configurator->setTempDirectory($appDir . '/temp');
 
 		$configurator->createRobotLoader()
 			->addDirectory(__DIR__)
 			->register();
 
-		$configurator->addConfig($appDir . '/config/common.neon');
-		$configurator->addConfig($appDir . '/config/services.neon');
-		$configurator->addConfig($appDir . '/config/local.neon');
+		$configurator->addConfig($appDir . '/config/main.neon');
 
 		return $configurator;
 	}
