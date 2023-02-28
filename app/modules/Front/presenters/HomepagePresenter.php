04c2fabb9c7b391968e57789b23fe6cc098581d8
Lukáš Vaculčík <lukas.vaculcik@gmail.com>
2023-02-28 17:34:23 +0100

    show animals by category at homepage
    Would load data from db with joins, but nette joins are weird :(

--- app/modules/Front/presenters/HomepagePresenter.php
+++ app/modules/Front/presenters/HomepagePresenter.php
@@ -8,15 +8,32 @@ use App\Model\AnimalRepository;
 
 final class HomepagePresenter extends BasePresenter
 {
-	public function __construct(
-		private readonly AnimalRepository $animalRepository,
-	)
-	{
+    public function __construct(
+        private readonly AnimalRepository $animalRepository,
+    ) {
         parent::__construct();
     }
 
-	public function renderDefault()
-	{
-		$this->template->animals = $this->animalRepository->findVisibleAnimals()->fetchAll();
-	}
+    public function renderDefault()
+    {
+        $animals = $this->animalRepository->findVisibleAnimals()->fetchAll();
+        $types = $this->animalRepository->findAnimalTypes()->fetchAll();
+
+        $animalHasType = $this->animalRepository->findAnimalHasType()->fetchPairs(AnimalRepository::COLUMN_HAS_TYPE_PARENT, AnimalRepository::COLUMN_HAS_TYPE_ID);
+
+        $grid = array_map(function ($type) use ($animalHasType) {
+            $animalIds = array_keys(
+                array_filter($animalHasType, function ($hasTypeId) use ($type) {
+                    return $type->id === $hasTypeId;
+                })
+            );
+            return [
+                'name' => $type->name,
+                'animals' => $animalIds,
+            ];
+        }, $types);
+
+        $this->template->animals = $animals;
+        $this->template->grid = $grid;
+    }
 }
